007858f9c22742e3c48a70b3577f0e00d4f54112
3real <mikegrahameverett@gmail.com>
2018-11-05 19:29:52 +0000

    begun tower script, much more complex to find nearest enemy than anticipated, will continue at alater date. code is functional

---
+++ TowerDefense/Assets/Scripts/Tower/Projectile.cs
@@ -0,0 +1,28 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public enum proType{
+	rock, arrow, fireball
+};
+public class Projectile : MonoBehaviour {
+
+[SerializeField]
+private int attackStrength;
+
+[SerializeField]
+private proType projectileType;
+
+
+public int AttackStrength{
+	get {
+		return attackStrength;
+	}
+}
+
+public proType ProjectileType{
+	get{
+		return projectileType;
+	}
+}
+}
---
+++ TowerDefense/Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+[SerializeField]
+private float timeBetweenAttacks;
+
+[SerializeField]
+private float attackRadius;
+
+private Projectile projectile;
+
+private Enemy targetEnemy = null;
+
+private float attackCounter;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//private Enemy GetNearestEnemyInRange(){
+
+	//}
+}

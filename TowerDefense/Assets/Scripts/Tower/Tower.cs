23e048bfadefe6bd57e4df1c6cebe7b9e9f2d145
3real <mikegrahameverett@gmail.com>
2018-11-19 20:30:28 +0000

    towers now shoot projectiles

--- TowerDefense/Assets/Scripts/Tower/Tower.cs
+++ TowerDefense/Assets/Scripts/Tower/Tower.cs
@@ -9,12 +9,14 @@ private float timeBetweenAttacks;
 
 [SerializeField]
 private float attackRadius;
-
+[SerializeField]
 private Projectile projectile;
 
 private Enemy targetEnemy = null;
 
 private float attackCounter;
+
+private bool isAttacking = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -22,13 +24,77 @@ private float attackCounter;
 	
 	// Update is called once per frame
 	void Update () {
+		attackCounter -= Time.deltaTime;
+
+		if (targetEnemy == null) {
+			Enemy nearestEnemy = GetNearestEnemyInRange();
+			if (nearestEnemy != null && Vector2.Distance(transform.localPosition, nearestEnemy.transform.localPosition) <= attackRadius){
+				targetEnemy = nearestEnemy;
+			} 
+		}
+		else {
+			if(attackCounter <= 0){
+				isAttacking = true;
+				// reset attackCounter.
+				attackCounter = timeBetweenAttacks;
+			}
+			else{
+				isAttacking = false;
+			}
+			if (Vector2.Distance(transform.localPosition, targetEnemy.transform.localPosition) > attackRadius){
+			targetEnemy = null;
+		}
+		}
+
 		
+	}
+
+	void FixedUpdate(){
+		if (isAttacking){
+			Attack();
+		}
+	}
+
+	public void Attack(){
+		isAttacking = false;
+		Projectile newProjectile = Instantiate(projectile) as Projectile;
+		newProjectile.transform.localPosition = transform.localPosition;
+		if (targetEnemy == null){
+			Destroy(newProjectile);
+		}
+		else{
+			//move projectile to enemy.
+			StartCoroutine(MoveProjectile(newProjectile));
+		}
+	}
+
+	IEnumerator MoveProjectile(Projectile projectile){
+		while (getTargetDistance(targetEnemy) > 0.2f && projectile != null  && targetEnemy != null) {
+			var dir = targetEnemy.transform.localPosition - transform.localPosition;
+			var angleDirection = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+			projectile.transform.rotation = Quaternion.AngleAxis(angleDirection, Vector3.forward);
+			projectile.transform.localPosition = Vector2.MoveTowards(projectile.transform.localPosition, targetEnemy.transform.localPosition, 5f * Time.deltaTime);
+			yield return null;
+		}
+		if (projectile != null || targetEnemy == null){
+			Destroy(projectile);
+		}
+	}
+
+	private float getTargetDistance(Enemy thisEnemy){
+		if (thisEnemy == null){
+			thisEnemy = GetNearestEnemyInRange();
+			if (thisEnemy == null){
+				return 0f;
+			}
+		}
+		return Mathf.Abs(Vector2.Distance(transform.localPosition, thisEnemy.transform.localPosition));
 	}
 
 	private List<Enemy> GetEnemiesInRange(){
 		List<Enemy> enemiesInRange = new List<Enemy>();
 		foreach(Enemy enemy in GameManager.Instance.EnemyList){
-			if (Vector2.Distance(transform.position, enemy.transform.position) <= attackRadius){
+			if (Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <= attackRadius){
 				enemiesInRange.Add(enemy);
 			}
 
@@ -39,8 +105,8 @@ private float attackCounter;
 		Enemy nearestEnemy = null;
 		float smallestDistance = float.PositiveInfinity;
 		foreach(Enemy enemy in GetEnemiesInRange()){
-			if(Vector2.Distance(transform.position, enemy.transform.position) <smallestDistance){
-				smallestDistance = Vector2.Distance(transform.position, enemy.transform.position);
+			if(Vector2.Distance(transform.localPosition, enemy.transform.localPosition) <smallestDistance){
+				smallestDistance = Vector2.Distance(transform.localPosition, enemy.transform.localPosition);
 				nearestEnemy = enemy;
 			}
 		}
